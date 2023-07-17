7fb75b8e2db733126f1cc1df9e85d561a84ee879
Ayoung <saing.sab@gmail.com>
2023-07-17 14:38:26 +0700

    adding key value into db

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, error::Error};
+use std::collections::HashMap;
 fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().unwrap();
@@ -7,21 +7,24 @@ fn main() {
     println!("The key is {}", value);
 
     // write file
-    let contents = format!("{}\t{}\n", key, value);
-    let write_result = std::fs::write("cmdrs.db", contents);
+    // let contents = format!("{}\t{}\n", key, value);
+    // let write_result = std::fs::write("cmdrs.db", contents);
     
     // Using Match partern to handling ERROR
-    match write_result {
-        Ok(()) => {
+    // match write_result {
+    //     Ok(()) => {
 
-        }
-        Err(e) => {
-            println!("Error : {}", e)
-        }
-    }
+    //     }
+    //     Err(e) => {
+    //         println!("Error : {}", e)
+    //     }
+    // }
 
     // Working with struct and method
-    let database = Database::new().expect("Creating db failed");
+    let mut database = Database::new().expect("Creating db failed");
+    database.insert(key.to_uppercase(), value.clone());
+    database.insert(key, value); 
+    database.flush().unwrap();
 }
 
 struct Database {
@@ -36,18 +39,31 @@ impl Database {
         //     Err(e) => {
         //         return Err(e);
         //     }
-        // };`
+        // };
         // Write an equal to above function
         let contens = std::fs::read_to_string("cmdrs.db")?;
         let mut map = HashMap::new();
 
         for line in contens.lines() {
-            let mut chunks = line.splitn(2, "\t");
+            let mut chunks = line.splitn(2, "\t"); //Split into two from the tap
             let key =  chunks.next().expect("No Key!");
             let value =  chunks.next().expect("No value!");
             map.insert(key.to_owned(), value.to_owned());
         }
         // populate our map
         Ok(Database { map: map})
+    }
+
+    fn insert(&mut self, key: String, values: String){
+        self.map.insert(key, values);
+    }
+
+    fn flush(self) -> std::io::Result<()> {
+        let mut contents = String::new();
+        for pairs in self.map {
+            let cmdpair = format!("{}\t{}\n", pairs.0, pairs.1);
+            contents.push_str(&cmdpair);
+        }
+        std::fs::write("cmdrs.db", contents)
     }
 }
